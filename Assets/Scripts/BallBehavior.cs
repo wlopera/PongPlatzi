2a6e0c3d3973ce111b4725db1d2ae536921f92c4
William Lopera <lopera.william@gmail.com>
2019-08-24 18:43:59 -0500

    Juego estudio PongPlatzi - Unity

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+
+public Transform paddle;
+public bool gameStarted = false;
+public Rigidbody2D rbBall; 
+float posDif;
+
+public AudioSource ballAudio;
+
+	// Use this for initialization
+	void Start () {
+		posDif = paddle.position.x - transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!gameStarted) {
+			
+			transform.position = new Vector3(paddle.position.x - posDif, paddle.position.y, paddle.position.z); 
+
+			if(Input.GetMouseButtonDown(0)) {
+				rbBall.velocity = new Vector2(9,9);
+				gameStarted = true;
+			}
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D other) {
+		ballAudio.Play();
+	}
+}
---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class DeadZone : MonoBehaviour
+{
+    // private void OnCollisionEnter2D(Collision2D collision) {
+    //  	Debug.Log("Collision");
+    // }
+
+    public Text scorePlayerText;
+    public Text scoreEnemyText;
+    int scorePlayerQuantity;
+    int scoreEnemyQuantity;
+    public SceneChanger sceneChanger;
+	public AudioSource deadZoneAudio;
+	public Transform followBall;
+
+    private void OnTriggerEnter2D(Collider2D ballCollision)
+    {
+        if (gameObject.tag == "Left")
+        {
+            scoreEnemyQuantity++;
+            UpdateScoreLabel(scoreEnemyText, scoreEnemyQuantity);
+        }
+        else if (gameObject.CompareTag("Right"))
+        {
+            scorePlayerQuantity++;
+            UpdateScoreLabel(scorePlayerText, scorePlayerQuantity);
+        }
+
+		ballCollision.GetComponent<BallBehavior>().gameStarted = false;
+
+		followBall.GetComponent<FollowBall>().speed += 0.03f;
+		
+		deadZoneAudio.Play();
+
+		CheckScore();
+    }
+
+    void CheckScore()
+    {
+        if (scorePlayerQuantity > 4)
+        {
+            sceneChanger.ChangeSceneTo("WinScene");
+        }
+        else if (scoreEnemyQuantity > 4)
+        {
+            sceneChanger.ChangeSceneTo("LoseScene");
+        }
+
+    }
+    void UpdateScoreLabel(Text label, int score)
+    {
+        label.text = score.ToString();
+    }
+
+}
